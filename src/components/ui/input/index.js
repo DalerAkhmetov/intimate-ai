7428798ff38c35540ab0ecbe2a622f83d7f6a2b2
vladislav <vladushqqqa@gmail.com>
2023-05-16 10:31:05 +0300

    added: ui components

---
+++ src/components/ui/input/index.js
@@ -0,0 +1,36 @@
+const $input = document.querySelectorAll('.input');
+const $field = document.querySelectorAll('.input__field');
+
+const toggleError = ($fieldCurrent, state, errorText) => {
+    const $inputCurrent = $fieldCurrent.parentNode;
+    const $errorCurrent = $inputCurrent.querySelector('.input__error');
+
+    $inputCurrent.classList.remove('is-error');
+    $errorCurrent.textContent = '';
+
+    if (state && errorText) {
+        $inputCurrent.classList.add('is-error');
+        $errorCurrent.textContent = errorText;
+    }
+};
+
+const changeHandler = (e) => {
+    e.target.value = e.target.value.trim();
+
+    e.target.parentNode.classList.toggle('is-filled', e.target.value);
+};
+
+const init = () => {
+    if (!$input.length) {
+        return;
+    }
+
+    $field.forEach(($fieldCurrent) => {
+        $fieldCurrent.addEventListener('change', changeHandler);
+    });
+};
+
+export default {
+    init,
+    toggleError,
+};
